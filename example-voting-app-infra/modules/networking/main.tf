02b5ea601c89d47714b4fdd35fe69651052bc0ef
Sidharth Sehrawat <sid2001sehrawat@gmail.com>
2026-06-13 18:58:07 +0530

    Add Cloud NAT and ArgoCD GitOps configuration

--- example-voting-app-infra/modules/networking/main.tf
+++ example-voting-app-infra/modules/networking/main.tf
@@ -20,3 +20,19 @@ resource  "google_compute_subnetwork" "subnet"  {
     ip_cidr_range = "10.30.0.0/20"
   }
 }
+
+resource "google_compute_router" "nat_router" {
+  name    = "voting-app-router"
+  region  = var.region
+  network = google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "voting-app-nat"
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+
+  nat_ip_allocate_option             = "AUTO_ONLY"
+
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
